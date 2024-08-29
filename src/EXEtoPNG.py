ff4d612c63d911b20cb38228628cbc06a92c3e27
Samuel Howard <sahoward42@tntech.edu>
2024-08-29 14:40:33 -0500

    Switch to numpy and apply parallelism

--- src/EXEtoPNG.py
+++ src/EXEtoPNG.py
@@ -1,26 +1,65 @@
-import GenPNG
-
-from os import listdir, path
-from sys import argv
-import threading
+import os
+import multiprocessing
+import time
 from typing import List
 
+import GenPNG
 
-def getDirectoryFiles(targetDirectory: str = "") -> List[str]:
-    fileList = []
-    # Get a list of files in a given directory
-    for file in listdir(targetDirectory):
-        fullFileName = targetDirectory + '/' + file
-        # Only take note of files, not directories
-        if(path.isfile(fullFileName)):
-            fileList.append(file)
-    return fileList
+
+CONVERSION_MODE = "L"
+
+
+def process_list(sub_file_list: List[str], in_dir: str, out_dir: str) -> None:
+    """
+    Process a given list of binary files.
+    :param sub_file_list: List of file paths.
+    :param in_dir: Input directory.
+    :param out_dir: Output Directory.
+    :return: None
+    """
+    global CONVERSION_MODE
+    for f in sub_file_list:
+        GenPNG.gen_png_numpy(f, f.replace(in_dir, out_dir), mode=CONVERSION_MODE)
 
 
 if __name__ == "__main__":
     inputDirectory = "input/"
     outputDirectory = "output/"
-    fileList = getDirectoryFiles(inputDirectory)
-    print("Found a total of " + str(len(fileList)) + " files to convert")
-    for file in fileList:
-        GenPNG.generateGrayScalePNG(inputDirectory + file, outputDirectory + file)
+
+    # Make sure the directories exist
+    assert os.path.isdir(inputDirectory), f"Input directory `{inputDirectory}` is not a directory"
+    assert os.path.isdir(outputDirectory), f"Output directory `{outputDirectory}` is not a directory"
+
+    # Scan them, printing the file count
+    file_list = [x.path for x in os.scandir(inputDirectory) if os.path.isfile(x.path)]
+    print("Found a total of " + str(len(file_list)) + " files to convert")
+
+    # Process initialization
+    num_processes = multiprocessing.cpu_count() // 2    # Don't use all threads to save some memory
+    pool = multiprocessing.Pool(processes=num_processes)
+
+    # Split the work to divide to the threads
+    if len(file_list) > num_processes:
+        split_file_list = [
+            file_list[i:i + num_processes]
+            for i in range(0, len(file_list), num_processes)
+        ]
+    else:
+        split_file_list = [
+            file_list[i:i + num_processes]
+            for i in range(0, len(file_list), 4)    # The step size can be decreased, but it doesn't matter much
+        ]
+
+    start = time.perf_counter()
+
+    # Add the work to the pool
+    for sub_list in split_file_list:
+        pool.apply_async(process_list, args=(sub_list, inputDirectory, outputDirectory))
+
+    # Start, do the work, and wait for results
+    pool.close()
+    pool.join()
+
+    end = time.perf_counter()
+
+    print(f"Conversion time: {end - start:.6f}s")
--- src/GenPNG.py
+++ src/GenPNG.py
@@ -1,69 +1,55 @@
 # === Module Imports ===================================================================================================
 from math import ceil
-from os import  path
+import numpy as np
+from os import path
 from PIL import Image
 from sys import argv
-from typing import List, Tuple
-
 
 # === Constant Definitions =============================================================================================
 DEFAULT_WIDTH = 512
 
 
 # === Functions ========================================================================================================
-def generateGrayScalePNG(inputFile: str, outputPNG: str, pngWidth: int = DEFAULT_WIDTH) -> None:
-    pngPixels = generateGrayScalePixelData(inputFile)
-    pngHeight = int(ceil(len(pngPixels) / pngWidth))
-    newImage = Image.new("RGB", (pngWidth, pngHeight), color="black")
-    newImage.putdata(pngPixels)
-    newImage.save(outputPNG + ".png", "PNG")
+def gen_png_numpy(input_path: str, output_path: str, width: int = DEFAULT_WIDTH, mode: str = "L") -> None:
+    """
+    Convert an input binary file to a PNG representation.
+    :param input_path: The path to the input binary.
+    :param output_path: The path to the output.
+    :param width: Width of the resulting image.
+    :param mode: Either "L" or "RGB" (RGB just duplicates "L")
+    :return: None
+    """
+    file_size: int = path.getsize(input_path)
+    if file_size < width:
+        height: int = 1
+    else:
+        height: int = ceil(file_size / width)
 
+    # Read the data as *serial* bytes (uint8)
+    data: np.ndarray = np.fromfile(input_path, dtype=np.uint8)
 
-def generateGrayScalePixelData(inputFile: str) -> List[int]:
-    pixelArray = []
-    fileSize = path.getsize(inputFile)
-    with open(inputFile, "rb") as exeIn:
-        # Iterate through file to get bytes in groups of 3
-        for n in enumerate(range(0, fileSize)):
-            pixelData = exeIn.read(1)
-            # Convert bytes into tuple of (R, G, B)
-            pixelData = (pixelData[0], pixelData[0], pixelData[0])
-            pixelArray.append(pixelData)
-    return pixelArray
+    # Pad the data to resize it
+    pad_amount: int = ((width * height) - file_size)
+    data = np.pad(
+        data,
+        pad_width=(0, pad_amount)
+    )
 
+    # Make it image shaped
+    data = np.reshape(data, (height, width))
 
-def generatePNG(inputFile: str, outputPNG: str, pngWidth: int = DEFAULT_WIDTH) -> None:
-    # Convert input file into pixels
-    pngPixels = generatePixelData(inputFile)
-    pngHeight = int(ceil(len(pngPixels) / pngWidth))
-    # Create new PNG image using data from file
-    newImage = Image.new("RGB", (pngWidth, pngHeight), color="white")
-    newImage.putdata(pngPixels)
-    newImage.save(outputPNG + ".png", "PNG")
-
-
-def generatePixelData(inputFile: str) -> List[Tuple[int, int, int]]:
-    pixelArray = []
-    fileSize = path.getsize(inputFile)
-    with open(inputFile, "rb") as exeIn:
-        # Iterate through file to get bytes in groups of 3
-        for n in range(0, ceil(fileSize / 3)):
-            pixelData = exeIn.read(3)
-            # Convert bytes into tuple of (R, G, B)
-            match (len(pixelData)):
-                # 255 substitutes for missing values
-                case 1:
-                    pixelData = (pixelData[0], 255, 255)
-                case 2:
-                    pixelData = (pixelData[0], pixelData[1], 255)
-                case 3:
-                    pixelData = (pixelData[0], pixelData[1], pixelData[2])
-                case _:
-                    pixelData = (255, 255, 255)
-            pixelArray.append(pixelData)
-    return pixelArray
+    # Use the appropriate mode
+    if mode == "RGB":
+        # Stack the data on itself to make the three channels
+        data = np.dstack((data, data, data))
+        img = Image.fromarray(data, mode="RGB")
+    else:
+        # Or just make it a PIL image
+        img = Image.fromarray(data, mode="L")
+    img.save(output_path + ".png")
+    img.close()
 
 
 # === Main =============================================================================================================
 if __name__ == "__main__":
-    generateGrayScalePNG(argv[1], argv[2])
+    gen_png_numpy(argv[1], argv[2])
